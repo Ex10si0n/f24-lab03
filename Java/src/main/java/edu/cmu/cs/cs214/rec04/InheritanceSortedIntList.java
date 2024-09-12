e67e670e4e6944470b5a9e534c7f5ab372f0a7d9
ex10si0n <yan.ex10si0n@icloud.com>
2024-09-12 16:14:25 -0400

    Added inheritance implementation of sorted integer list class, test cases passed

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -7,10 +7,48 @@ package edu.cmu.cs.cs214.rec04;
  * and exports an accessor (totalAdded) for this count.
  *
  * @author Nora Shoemaker
- *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList {
+
+    private int totalAdded;
+
+    public InheritanceSortedIntList() {
+        super();
+        this.totalAdded = 0;
+    }
+
+    /**
+     * Adds an integer to the list and increments the totalAdded count
+     * @param num an integer to be added to the list
+     * @return true if the integer was added to the list, false otherwise
+     */
+    @Override
+    public boolean add(int num) {
+        if (super.add(num)) {
+            this.totalAdded++;
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * Adds all elements from the given IntegerList to the list and increments the totalAdded count
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if all elements were added to the list, false otherwise
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+//        this.totalAdded += list.size();
+        return super.addAll(list);
+    }
+
+    /**
+     * Get the total number of elements added to the list
+     * @return the total number of elements added to the list
+     */
+    public int getTotalAdded() {
+        return this.totalAdded;
+    }
 
 }
