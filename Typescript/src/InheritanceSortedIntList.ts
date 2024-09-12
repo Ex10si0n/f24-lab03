d29aac05777f6dedc318d3a083e5b7be06491a4b
ex10si0n <yan.ex10si0n@icloud.com>
2024-09-12 16:52:00 -0400

    (ts) Added inheritance implementation of sorted integer list class, test cases passed

--- Typescript/src/InheritanceSortedIntList.ts
+++ Typescript/src/InheritanceSortedIntList.ts
@@ -1,4 +1,5 @@
-import { SortedIntList } from './hidden/SortedIntListLibrary.js'
+import {SortedIntList} from './hidden/SortedIntListLibrary.js'
+import {IntegerList} from "./IntegerList";
 
 /**
  * InheritanceSortedIntList -- a variant of a SortedIntList that keeps
@@ -10,9 +11,48 @@ import { SortedIntList } from './hidden/SortedIntListLibrary.js'
  *
  */
 
-class InheritanceSortedIntList {
-  // Write your implementation below with API documentation
+class InheritanceSortedIntList extends SortedIntList {
 
+  // The total number of elements added to the list.
+  private totalAdded: number
+
+  /**
+   * Construct a new InheritanceSortedIntList object.
+   */
+  constructor() {
+    super();
+    this.totalAdded = 0
+  }
+
+  /**
+   * Adds a number to the list and increments totalAdded if successful.
+   * @param num a number to be added
+   * @return true if the number was added, false otherwise
+   */
+  add(num: number): boolean {
+    if (super.add(num)) {
+      this.totalAdded++
+      return true
+    }
+    return false
+  }
+
+  /**
+   * Adds all elements from the given list and increments totalAdded accordingly.
+   * @param list
+   * @return true if all elements were added, false otherwise
+   */
+  addAll(list: IntegerList): boolean {
+    return super.addAll(list);
+  }
+
+  /**
+   * Get the total number of elements added to the list.
+   * @return the total number of elements added to the list
+   */
+  getTotalAdded(): number {
+    return this.totalAdded;
+  }
 }
 
-export { InheritanceSortedIntList }
+export {InheritanceSortedIntList}
